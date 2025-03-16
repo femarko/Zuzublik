083091cb010f2df711a47ab631d4baff33450705
femarko <fenstermark7@gmail.com>
2025-03-17 00:07:40 +0100

    Bot: "File uploaded successfully!" message done.

--- app/entrypoints/bot.py
+++ app/entrypoints/bot.py
@@ -11,4 +11,23 @@ def start(message):
     bot.send_message(message.chat.id, text='Hello! Want to upload your zuzubles?', reply_markup=markup)
 
 
+@bot.callback_query_handler(func=lambda call: True)
+def handle_callback(call):
+    if call.data == 'upload_excel':
+        callback_upload_excel(call)
+
+
+def callback_upload_excel(call):
+    bot.send_message(call.message.chat.id, 'Please upload your Excel file')
+    bot.register_next_step_handler(call.message, handle_file)
+
+
+def handle_file(message):
+    file_id = message.document.file_id
+    file = bot.get_file(file_id)
+    downloaded_file = bot.download_file(file.file_path)
+    bot.send_message(message.chat.id, 'File uploaded successfully!')
+
+
+
 bot.polling(none_stop=True)
