0641c84769ea4a1beee4b47616086d4e46ce526c
femarko <fenstermark7@gmail.com>
2025-03-17 13:51:09 +0100

    app/service_layer/app_manager.add_zuzublik modified.

--- app/service_layer/app_manager.py
+++ app/service_layer/app_manager.py
@@ -5,9 +5,10 @@ from app.auxiliary_services.parser import parse_table
 def add_zuzublik(file, parser, validator, uow) -> int:
     parsed_data = parser(file)
     validated_data = validator(**parsed_data)
-    zuz_instance = models.create_zuzublik(**validated_data)
     with uow:
-        uow.zuzublik_repo.add(zuz_instance)
+        for item in validated_data:
+            zuz_instance = models.create_zuzublik(**item)
+            uow.zuzublik_repo.add(zuz_instance)
         uow.commit()
-        zuz_instance_id: int = zuz_instance.id
+    zuz_instance_id: int = zuz_instance.id
     return zuz_instance_id
