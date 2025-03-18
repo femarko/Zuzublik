28cd7bedd78148af426eecab590f1edd29218948
femarko <fenstermark7@gmail.com>
2025-03-18 12:01:55 +0100

    app/auxiliary_services/parser.py modified.

--- app/auxiliary_services/parser.py
+++ app/auxiliary_services/parser.py
@@ -1,18 +1,11 @@
 import io
 import pathlib
-from typing import Iterable, Optional
 from pandas import read_excel
 
 
 file_path = pathlib.Path(__file__).parent.parent.parent / "test_file.xls"
 
-def parse_table(table_file, fields: Optional[Iterable] = None):
+def parse_table(table_file):
     df = read_excel(io.BytesIO(table_file))
-    # result: dict[str, str] = {}
-    if fields is not None:
-        columns = fields
     result = df.to_dict(orient="records")
-    # for row_index in range(len(df)):
-    #     for column in df.columns:
-    #         result |= {column: df[column][row_index]}
     return result
